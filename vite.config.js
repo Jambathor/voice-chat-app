319f2195ce6f16aebcd258408b94332375d56194
Jambathor <yassinereguigui@gmail.com>
2024-12-12 17:00:06 +0100

    Update deployment workflow and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Voice-Chat-Rooms/',
+  base: '/voice-chat-app/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
